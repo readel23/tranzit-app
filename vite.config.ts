a062bcfe7b166c74c9c3aea64565aee3e9d61cd8
readel <133009813+readel23@users.noreply.github.com>
2026-06-11 11:07:13 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(() => {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
+    base: "/tranzit-app/",
     },
     server: {
       // HMR is disabled in AI Studio via DISABLE_HMR env var.
